8766e46468f9187997c982bad4d94d39221d1785
raulriverarojas <72164424+raulriverarojas@users.noreply.github.com>
2022-12-28 23:24:19 +0000

    commit7?

--- PrimitiveDatatypes.java
+++ PrimitiveDatatypes.java
@@ -3,7 +3,7 @@ public class PrimitiveDatatypes {
   public static void main(String[] args){
     int num=58;
     System.out.println(num);
-    System.out.println("runs question mark");
+    System.out.println("passes tests?");
   //code here
   }
 }
